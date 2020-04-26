893b6887e4aff36118a7ba6ad276a8dc221b6a8c
alteregoprofile <42719719+alteregoprofile@users.noreply.github.com>
2020-04-26 20:22:59 +0530

    Create daytempngreetings.py

---
+++ daytempngreetings.py
@@ -0,0 +1,13 @@
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("its's a hot day")
+    print('drink water')
+
+elif is_cold:
+    print('is very cold')
+    print('stay warm')
+else:
+    print('lovely day')
+print('enjoiy the day')
